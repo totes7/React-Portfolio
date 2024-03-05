aa7d74edea29133dce2989a2fa90c30c75f6d477
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-05 13:29:08 +0000

    create buttons links in home page; first styling of buttons

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,6 +9,19 @@ function Home() {
           <h1 className="display-4">Antonio Manno</h1>
           <h2>Junior Full-Stack Web Developer</h2>
         </div>
+        <div className="btn-links">
+          <button type="button" className="btn btn-outline-secondary">
+            <i className="fa-solid fa-question"></i>
+            <br></br>
+            About
+          </button>
+          <button type="button" className="btn btn-outline-secondary">
+            Portfolio
+          </button>
+          <button type="button" className="btn btn-outline-secondary">
+            Contact
+          </button>
+        </div>
       </div>
     </div>
   );
