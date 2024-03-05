69ab2e0f5952fce5951db71af92d22f7b1b8869f
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-05 12:42:15 +0000

    create hero section in home.jsx and style the title

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./styles/Home.css";
 
 function Home() {
-    return (
+  return (
+    <div>
+      <div className="hero">
         <div>
-            Home
+          <h1 className="display-4">Antonio Manno</h1>
+          <h2>Junior Full-Stack Web Developer</h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
