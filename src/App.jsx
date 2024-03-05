90be60ce192f0bccdafd001e7dbc9245b39afa61
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-05 12:15:41 +0000

    create routes in app.jsx and each component file; connect each components route

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <Router>
       <NavTabs />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
