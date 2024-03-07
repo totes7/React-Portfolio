cf3837725ed8aa678f8fed504f92ae2fe71d0481
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-07 16:12:47 +0000

    complete portfolio page and add skill section in about page

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -5,7 +5,7 @@ function PortfolioCard(props) {
   return (
     <>
       <div class="card text-bg-dark">
-        <img src={props.image} class="card-img-top" alt="..." />
+        <img src={props.image} class="card-img-top" alt="website preview" />
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
           <p class="card-text">
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/Portfolio.css";
 import PortfolioCard from "../PortfolioCard.jsx";
 
@@ -14,34 +15,34 @@ function Portfolio() {
             </div>
           </div>
           <div className="row cards-section">
-            <div className="col-lg-3">
+            <Link target="_blank" to='https://totes7.github.io/project-portfolio-one/' role="button" className="col-lg-3 card-wrapper">
               <PortfolioCard
                 image="./src/images/portfolio-1.jpg"
                 title="Basketball Tournament"
                 text="Landing and registration pages made with html and css."
               />
-            </div>
-            <div className="col-lg-3">
+            </Link>
+            <Link target="_blank" to='https://totes7.github.io/project-portfolio-two/' role="button" className="col-lg-3 card-wrapper">
               <PortfolioCard 
                 image="./src/images/portfolio-2.jpg"
                 title="FIFA WC Quiz"
                 text="Interactive website made with html, css and javascript."
               />
-            </div>
-            <div className="col-lg-3">
+            </Link>
+            <Link target="_blank" to='https://totes7-dinemate.herokuapp.com/' role="button" className="col-lg-3 card-wrapper">
               <PortfolioCard 
                 image="./src/images/portfolio-3.jpg"
                 title="DineMate"
                 text="Restaurant website built with Django framework."
               />
-            </div>
-            <div className="col-lg-3">
+            </Link>
+            <Link target="_blank" to='https://heritage-housing-pp5-7e5087ac3438.herokuapp.com/' role="button" className="col-lg-3 card-wrapper">
               <PortfolioCard 
                 image="./src/images/portfolio-4.jpg"
                 title="Heritage Housing Study"
                 text="Data Analytics reserch with Jupyter in a Streamlit app."
               />
-            </div>
+            </Link>
           </div>
         </div>
       </div>
