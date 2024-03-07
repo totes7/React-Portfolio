ea0b6aaa8642e27064b041958ad7b081e71ede5c
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-07 16:13:06 +0000

    complete portfolio page and add skill section in about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,6 +10,7 @@ function About() {
           <div className="col-lg-5 title-about">
             <h1 className="display-4">Antonio Manno</h1>
             <h4>Junior Full-Stack Web Developer</h4>
+            <hr />
             <Link to="/portfolio" role="button" className="button">
               <i className="fa-regular fa-folder"></i>
               <br></br>
@@ -18,33 +19,18 @@ function About() {
           </div>
           <div className="col-lg-7 text">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              tempora rerum iusto facilis hic minus totam amet incidunt ullam
-              quibusdam et cumque vero blanditiis optio delectus, aperiam
-              nesciunt expedita nihil. Distinctio beatae, commodi labore
-              praesentium aperiam accusantium quos iste expedita voluptatum in
-              molestiae natus maxime quis optio molestias porro unde non
-              similique rerum, nulla repellendus veniam, facilis voluptates.
-              Eos, doloremque? Temporibus ab possimus, officiis tempora
-              reiciendis iusto earum aperiam perspiciatis eius amet consequatur.
-              Ipsum odit deleniti minus facere hic accusantium cum, pariatur
-              nemo iure, saepe ad impedit nostrum voluptatum in? Similique,
-              earum recusandae omnis architecto sint, magnam fugit ex, hic at
-              perspiciatis a! Eligendi eaque exercitationem perferendis ut,
-              consequuntur aperiam ducimus a sint excepturi temporibus dolores
-              atque recusandae quae expedita. Dignissimos necessitatibus autem
-              non? Quidem, dolorem! Obcaecati facilis nobis suscipit temporibus
-              perspiciatis ut, expedita possimus distinctio, cum officiis
-              reprehenderit repellendus non nihil quis vitae incidunt, dolorum
-              dolore vel natus perferendis? Corporis blanditiis debitis ea
-              necessitatibus minus obcaecati culpa vel optio! Totam porro culpa,
-              ut eum asperiores repudiandae praesentium inventore alias repellat
-              odio harum modi aliquam adipisci beatae id minus facere. Voluptas,
-              quo. Sit quidem optio alias ipsum earum? Nostrum error provident
-              perspiciatis exercitationem doloribus laborum neque, consequuntur
-              aut dolorum at voluptate explicabo nam sapiente, ad, ea odio
-              recusandae voluptates nobis.
+            Highly motivated and freshly certified Full Stack Web Developer with a specialization in Front End development. Eager to leverage my newly acquired skills and knowledge to contribute effectively to dynamic web development projects. Committed to delivering innovative, user-centric solutions while continuously enhancing my proficiency in cutting-edge technologies.
             </p>
+            <h4>Skills</h4>
+            <hr />
+            <ul>
+              <li><strong>Technologies:</strong> HTML5, CSS3, JavaScript, Python</li>
+              <li>
+              <strong>Frameworks:</strong> jQuery, React, Bootstrap, Django, Flask, Streamlit
+              </li>
+              <li><strong>Databases:</strong> PostgreSQL, MySQL</li>
+              <li><strong>Version Control:</strong> Git, Github</li>
+            </ul>
           </div>
         </div>
       </div>
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -15,34 +15,73 @@ function Portfolio() {
             </div>
           </div>
           <div className="row cards-section">
-            <Link target="_blank" to='https://totes7.github.io/project-portfolio-one/' role="button" className="col-lg-3 card-wrapper">
+            <Link
+              target="_blank"
+              to="https://totes7.github.io/project-portfolio-one/"
+              role="button"
+              className="col-lg-3 card-wrapper"
+            >
               <PortfolioCard
                 image="./src/images/portfolio-1.jpg"
                 title="Basketball Tournament"
                 text="Landing and registration pages made with html and css."
               />
             </Link>
-            <Link target="_blank" to='https://totes7.github.io/project-portfolio-two/' role="button" className="col-lg-3 card-wrapper">
-              <PortfolioCard 
+            <Link
+              target="_blank"
+              to="https://totes7.github.io/project-portfolio-two/"
+              role="button"
+              className="col-lg-3 card-wrapper"
+            >
+              <PortfolioCard
                 image="./src/images/portfolio-2.jpg"
                 title="FIFA WC Quiz"
                 text="Interactive website made with html, css and javascript."
               />
             </Link>
-            <Link target="_blank" to='https://totes7-dinemate.herokuapp.com/' role="button" className="col-lg-3 card-wrapper">
-              <PortfolioCard 
+            <Link
+              target="_blank"
+              to="https://totes7-dinemate.herokuapp.com/"
+              role="button"
+              className="col-lg-3 card-wrapper"
+            >
+              <PortfolioCard
                 image="./src/images/portfolio-3.jpg"
                 title="DineMate"
                 text="Restaurant website built with Django framework."
               />
             </Link>
-            <Link target="_blank" to='https://heritage-housing-pp5-7e5087ac3438.herokuapp.com/' role="button" className="col-lg-3 card-wrapper">
-              <PortfolioCard 
+            <Link
+              target="_blank"
+              to="https://heritage-housing-pp5-7e5087ac3438.herokuapp.com/"
+              role="button"
+              className="col-lg-3 card-wrapper"
+            >
+              <PortfolioCard
                 image="./src/images/portfolio-4.jpg"
                 title="Heritage Housing Study"
                 text="Data Analytics reserch with Jupyter in a Streamlit app."
               />
             </Link>
+          </div>
+          <div className="row projects-link">
+            <div className="col-lg-12">
+              <h4 className="link-text">
+                Check out the rest of my projects on my GitHub profile
+              </h4>
+            </div>
+            <div className="col-lg-12 button-row">
+              <Link
+                target="_blank"
+                to="https://github.com/totes7?tab=repositories"
+                role="button"
+                className="link-button"
+              >
+                <i class="fa-brands fa-github"></i>
+                <br />
+                GuitHub
+              </Link>
+            </div>
           </div>
         </div>
       </div>
