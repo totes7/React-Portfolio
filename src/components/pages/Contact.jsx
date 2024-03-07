d8906f58baf46eeb254db65ad6ff87478fa09086
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-07 19:59:07 +0000

    create contact page title

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./styles/Contact.css";
 
 function Contact() {
-    return (
-        <div>
-            Contact
+  return (
+    <>
+      <div className="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 contact-title">
+              <h1>Contact</h1>
+              <hr />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Contact;
