ce8543da2887c798f516ee43fad3a423d96b4547
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-08 22:42:06 +0000

    change navbar to make it more responsive to screen size

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,62 +1,75 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavTabs.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavTabs.css";
 
 function NavTabs() {
   return (
-    <ul className="nav nav-tabs">
-      <li>
-        <NavLink
-          to="/"
-          end
-          className='logo'
-        >
-          Antonio/&gt;
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="about"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="portfolio"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Portfolio
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="contact"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Contact
-        </NavLink>
-      </li>
-    </ul>
+    <>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <NavLink to="/" end className="logo">
+            Antonio/&gt;
+          </NavLink>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <NavLink
+                  to="/"
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink
+                  to="about"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink
+                  to="portfolio"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Portfolio
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink
+                  to="contact"
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
